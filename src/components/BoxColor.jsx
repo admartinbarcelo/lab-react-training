e75f55d9d7338c1f4214a178356a1d087d8a88c7
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-02-28 19:41:11 +0100

    iteracion 1 a 7 y con algo de css

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -2,6 +2,9 @@ function BoxColor({ r, g, b }) {
   const color = `rgb(${r} ${g} ${b})`;
   const style = {
     backgroundColor: color,
+    width: "400px",
+    height: "100px",
+    margin: "10px",
   };
   return (
     <div className="box-color" style={style}>
